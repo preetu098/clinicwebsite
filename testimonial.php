86b5db13991b3685e71c8ef871fe7bf48980ff11
Preetu098 <preetu098@gmail.com>
2023-01-26 09:52:07 +0530

    add changes

--- testimonial.php
+++ testimonial.php
@@ -1,224 +1,155 @@
 <?php include('header.php');?>
 <style>
-        .cardmove:hover 
-        {
-            transform:translateY(-10px);
-            transition:all 5s;
-        }
-        .card1 
-        {
-            height:80px;
-            border-radius:40px;
-            width:80px;
-            color:white;
-            background-color:orange;
-            margin-left:100px;
-           
-        }
-        /* @keyframes moveleft
-        {
-            from{transform:translateX(0px);}
-            to{transform:translateX(120px);}
-        } */
-        @keyframes moveleft1
-        {
-            from{transform:rotate(0deg);}
-            to{transform:rotate(120deg);}
-        }
-       /* .card1
-       {
-            animation-name:moveleft1;
-            animation-duration:15s;
-            animation-iteration-count:infinite;
-       } */
-      
-       
+   button 
+   {
+    width:150px;
+    height:40px;
+    background-color:#133e85;
+    color:white;
+    border:0px;
+    font: normal 1.2em/1.7em Verdana, 'Trebuchet MS', Tahoma, sans-serif;
+    border-radius:5px;
+   }
+   button:hover 
+   {
+    border:1px solid #133e85;
+    background-color:white;
+    color:#133e85;
+   }
+   .card
+   {
+    box-shadow:2px 2px 12px gray;
+   }
 </style>
-<div class="container-fluid global-padding-0">
-<div class="container-fluid testm_back_bg_color">
-<div class="container banner-text_tstm testimonial_banner">
-<div class="clear-fix">&nbsp;</div>
-<div class="row">
-<div class="col-12  text-center">
-<div class="tstm_banner_txt">
-<h1 class="text-center">Welcome in <span style="color: #414042;">PEOPLE</span><br />
-<span style="color: #414042;">Thoughts</span> Lives</h1>
-<img alt="" src="/sites/default/files/patienttestimonials/healing_pillman_border.png" /></div>
-</div>
-</div>
-<div class="clear-fix">&nbsp;</div>
-</div>
-</div>
-</div>
-
 <section>
-    <div class="container ">
-       
-        <div class="row mt-5 mb-5">
-                <div class="col-lg-4 col-4">
-                    <div class="text-center">
-                    <h2 class="mt-5 mb-5">Happy Client</h2>
-                    </div>
-                    <div class="card bg-light cardmove">
-                        <div class="card-body">
-
-                            <div class="text-center">
-                                
-                                <img src="https://homeopathy247.com/wp-content/uploads/2021/09/homeopathy247-client-membership.jpg" alt="" style="height:300px;width:100%;">
-                                <h2 class="mt-5 mb-5">Welcome Slide1</h2>
-                                <button class="btn btn-warning text-white">View ALL</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-4 mt-5">
-                <div class="text-center">
-                    <h2 class="mt-5 mb-5">Happy Client</h2>
-                    </div>
-                <div class="card bg-light cardmove">
-                        <div class="card-body">
-                            <div class="text-center">
-                            <img src="https://www.homeopathy360.com/wp-content/uploads/homeopathyforhairloss-homeopathy360.jpg" alt="" style="height:300px;width:100%;">
-                                <h2 class="mt-5 mb-5">Welcome Slide1</h2>
-                                <button class="btn btn-warning text-white">View ALL</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-4">
-                <div class="text-center">
-                    <h2 class="mt-5 mb-5">Happy Client</h2>
-                    </div>
-                <div class="card bg-light cardmove">
-                        <div class="card-body">
-                            <div class="text-center">
-                            <img src="https://www.welcomecure.com/assets/images/disease_conditions/Parkinsons/Parkinsons_homeopathic_treatment.jpg" alt="" style="height:300px;width:100%;">
-                                <h2 class="mt-5 mb-5">Welcome Slide1</h2>
-                                <button class="btn btn-warning text-white">View ALL</button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <div class="container-fluid" style="background-image:url('https://parumalahospital.com/assets/images/inside-header/about.jpg');background-repeat:no-repeat;background-size:cover">
+        <div class="text-center">
+            <h2 class="mt-5 mb-5" style="margin-top:200px!important;font-size:30px!important">Testimonial</h2>
         </div>
     </div>
 </section>
-<hr>
-<section>
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-3">
-                
-            </div>
-            <div class="col-lg-3">
-
-            </div>
-            <div class="col-lg-3">
-
-            </div>
-            <div class="col-lg-3">
-
-            </div>
-        </div>
-    </div>
-</section>
-
-
-
-
-
-
-
-<hr>
 <section>
     <div class="container">
         <div class="text-center">
-            <h1 style="font-size:30px!important;">Testimonial</h1>
-        </div>
-        <div class="row mt-5">
-            <div class="col-lg-5">
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-        <div class="card" style="background-image:url('https://img1.picmix.com/output/stamp/normal/7/4/3/4/534347_0e653.gif')">
-                            <div class="text-center">
-                                <img class="mt-5 mb-3" src="https://lanarkshirewebsitedesign.co.uk/wp-content/uploads/2017/10/lanarkshire-website-design-happy-client.png" alt="" style="height:100px;width:100px;border-radius:500px;">
-                            </div>
-                <div class="card-body bg-danger" style="border-top-left-radius:60px;border-top-right-radius:60px">
-                    <div class="text-center">
-                        <h3>User1</h3>
-                    </div>
-                    <p class="text-white" style="font-size:17px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolorum, maiores quos ea voluptate incidunt, error, quibusdam a aut fuga ipsa quo molestiae? Corporis molestiae ipsum vitae animi tenetur deleniti.</p>
-                    <p class="text-white" style="font-size:17px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolorum, maiores quos ea voluptate incidunt, error, quibusdam a aut fuga ipsa quo molestiae? Corporis molestiae ipsum vitae animi tenetur deleniti.</p>
-                    
+            <h1 class="mt-5 mb-5">Testimonial</h1>
+            <div class="row">
+                <div class="col-lg-4 col-4">
+            <button>All</button>
                 </div>
-      </div>
-    </div>
-    <div class="carousel-item">
-    <div class="card" style="background-image:url('https://img1.picmix.com/output/stamp/normal/7/4/3/4/534347_0e653.gif')">
-                            <div class="text-center">
-                                <img class="mt-5 mb-3" src="https://media.istockphoto.com/id/995734014/photo/businessman-business-woman-working-together.jpg?s=612x612&w=0&k=20&c=BnEf7QBC-ixI4VyBEpw2yI24gDEyXQ1slgrqRrBi7QE=" alt="" style="height:100px;width:100px;border-radius:500px;">
-                            </div>
-                <div class="card-body bg-primary" style="border-top-left-radius:60px;border-top-right-radius:60px">
-                    <div class="text-center">
-                        <h3>User1</h3>
-                    </div>
-                    <p class="text-white" style="font-size:17px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolorum, maiores quos ea voluptate incidunt, error, quibusdam a aut fuga ipsa quo molestiae? Corporis molestiae ipsum vitae animi tenetur deleniti.</p>
-                    <p class="text-white" style="font-size:17px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolorum, maiores quos ea voluptate incidunt, error, quibusdam a aut fuga ipsa quo molestiae? Corporis molestiae ipsum vitae animi tenetur deleniti.</p>
-                   
+                <div class="col-lg-4 col-4">
+            <button>Recent</button>
                 </div>
-      </div>
-    </div>
-    <div class="carousel-item">
-    <div class="card" style="background-image:url('https://img1.picmix.com/output/stamp/normal/7/4/3/4/534347_0e653.gif')">
-                            <div class="text-center">
-                                <img class="mt-5 mb-3" src="https://media.istockphoto.com/id/623427218/photo/beautiful-girl-texting-in-cafe.jpg?b=1&s=170667a&w=0&k=20&c=pD_pgw_J9Qqhk3SbDFNYnjjDxB-Via_rvLyFVKtqQyA=" alt="" style="height:100px;width:100px;border-radius:500px;">
-                            </div>
-                <div class="card-body bg-warning" style="border-top-left-radius:60px;border-top-right-radius:60px">
-                    <div class="text-center">
-                        <h3>User1</h3>
-                    </div>
-                    <p class="text-white" style="font-size:17px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolorum, maiores quos ea voluptate incidunt, error, quibusdam a aut fuga ipsa quo molestiae? Corporis molestiae ipsum vitae animi tenetur deleniti.</p>
-                    <p class="text-white" style="font-size:17px">Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dolorum, maiores quos ea voluptate incidunt, error, quibusdam a aut fuga ipsa quo molestiae? Corporis molestiae ipsum vitae animi tenetur deleniti.</p>
-                
+                <div class="col-lg-4 col-4">
+            <button>Old</button>
                 </div>
-      </div>
-    </div>
-  </div>
- 
-</div>
-            </div>
-            <div class="col-lg-7">
-                        <div class="row">
-                            <div class="col-lg-2"></div>
-                            <div class="col-lg-8">
-                            <div id="carouselExampleIndicators1" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators1" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators1" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators1" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" style="height:400px;width:100%" src="assets/slide1.jpg" alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" style="height:400px;width:100%" src="assets/slide4.jpg" alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="assets/slide5.jpg" style="height:400px;width:100%" alt="Third slide">
-    </div>
-                            </div>
-                            <div class="col-lg-2"></div>
-                        </div>
+               
             </div>
         </div>
     </div>
 </section>
-
+<section class="mt-5    ">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="text-center">
+                        <img src="assets/pat1.png" style="height:200px;width:200px" alt="">
+                        <h1>User1</h1>
+                        <p class="ms-5 me-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sunt.</p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div class="col-lg-4">
+<div class="card">
+<div class="text-center">
+                        <img src="assets/pat1.png" style="height:200px;width:200px" alt="">
+                        <h1>User1</h1>
+                        <p class="ms-5 me-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sunt.</p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div class="col-lg-4">
+            <div class="card">
+            <div class="text-center">
+                        <img src="assets/pat1.png" style="height:200px;width:200px" alt="">
+                        <h1>User1</h1>
+                        <p class="ms-5 me-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sunt.</p>
+                    </div>
+                   
+                    </div>
+            </div>
+            
+        </div>
+        <div class="row mt-5">
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="text-center">
+                        <img src="assets/pat1.png" style="height:200px;width:200px" alt="">
+                        <h1>User1</h1>
+                        <p class="ms-5 me-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sunt.</p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div class="col-lg-4">
+<div class="card">
+<div class="text-center">
+                        <img src="assets/pat1.png" style="height:200px;width:200px" alt="">
+                        <h1>User1</h1>
+                        <p class="ms-5 me-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sunt.</p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div class="col-lg-4">
+            <div class="card">
+            <div class="text-center">
+                        <img src="assets/pat1.png" style="height:200px;width:200px" alt="">
+                        <h1>User1</h1>
+                        <p class="ms-5 me-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sunt.</p>
+                    </div>
+                   
+                    </div>
+            </div>
+            
+        </div>
+        <div class="row mt-5">
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="text-center">
+                        <img src="assets/pat1.png" style="height:200px;width:200px" alt="">
+                        <h1>User1</h1>
+                        <p class="ms-5 me-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sunt.</p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div class="col-lg-4">
+<div class="card">
+<div class="text-center">
+                        <img src="assets/pat1.png" style="height:200px;width:200px" alt="">
+                        <h1>User1</h1>
+                        <p class="ms-5 me-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sunt.</p>
+                    </div>
+                   
+                </div>
+            </div>
+            <div class="col-lg-4">
+            <div class="card">
+            <div class="text-center">
+                        <img src="assets/pat1.png" style="height:200px;width:200px" alt="">
+                        <h1>User1</h1>
+                        <p class="ms-5 me-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. A, sunt.</p>
+                    </div>
+                   
+                    </div>
+            </div>
+            
+        </div>
+        
+    </div>
+</section>
 
 <?php include('footer.php');?>
